d373019b9213a82d0e6b6caffc940e152183d7d8
Vlad <andreivladpaul@gmail.com>
2021-07-24 16:45:39 +0200

    Modifica al codice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// PAROLA PALINDROMA
 //Chiedere all’utente di inserire una parola
 //var str = prompt("Inserisci parola palindroma");
 //Creare una funzione per capire se la parola inserita è palindroma
@@ -5,42 +6,46 @@
     var myArr = str.split('').reverse().join('');
     
     if (myArr == str) {
-        console.log("La parole è palindroma")
+        console.log("La parole è palindroma");
     } else {
         console.log("La parola non è palindroma");
     }
    
 }
-
 parolaPali(str); */
-
+//---------------------------------------------------------------------------------
+// PARI O DISPARI
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 var pariDispari = prompt("Scegli tra pari o dispari");
 
-function sceltaUtente (pariDispari) {
-    if (pariDispari == "pari") {
-        /* var scegliNumero = prompt("Scegli un numero tra: 2 e 4."); */
-        return(pariDispari);
+function func(str) {
+    var string = str.toUpperCase();
 
-        
+    if (string == "PARI") {
+        /* var scegliNumero = prompt("Scegli un numero tra: 2 e 4."); */
+        return(string);
+
     } else {
          /* scegliNumero = prompt("Scegli un numero tra: 1, 3 e 5."); */
-         return(pariDispari);
+         return(string);
     }
-    
 }
 
-console.log(pariDispari);
+func(pariDispari);
+console.log(func(pariDispari));
+
 
 var numeroScelto = parseInt(prompt("Scegli un numero tra 1 e 5"));
 console.log(numeroScelto);
-
+//---------------------------------------------------------------------------------
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 var numCM =  Math.floor(Math.random() * 5) + 1;
 console.log(numCM);
+//---------------------------------------------------------------------------------
 //Sommiamo i due numeri
 var somma = numeroScelto + numCM;
 console.log("la somma è: " + somma);
+//---------------------------------------------------------------------------------
 //Stabiliamo se la somma dei due numeri se è pari o dispari (usando una funzione)
 
 function sommaPariDispari(scelta, num, num2) {
